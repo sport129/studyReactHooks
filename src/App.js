e3170108dd6294bf60d2ba7669f7f07d16fcd558
Daniel <daniel.rosendos@hotmail.com>
2020-02-12 23:20:00 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-function App() {
+import './config/ReactotronConfig';
+
+import Header from './components/Header';
+import Routes from './routes';
+import history from './services/history';
+import store from './store';
+import GlobalStyle from './styles/global';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} />
+      </Router>
+    </Provider>
   );
 }
-
-export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MdShoppingBasket } from 'react-icons/md';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import Logo from '../../assets/images/logo.svg';
+import { Container, Cart } from './styles';
+
+export default function Header() {
+  const cartSize = useSelector(state => state.cart.length);
+
+  return (
+    <Container>
+      <Link to="/">
+        <img src={Logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu Carrinho</strong>
+          <span>{cartSize} Itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
+    </Container>
+  );
+}
